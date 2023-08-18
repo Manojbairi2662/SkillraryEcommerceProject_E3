a5c0060adb0dc54b73908a575849e762becfac2f
Manoj <Manoj@Manoj>
2023-08-18 18:01:28 +0530

    Changes of project new child

---
+++ Basic_Selenium_E3/src/test/java/Basic_Programs/Firefox_Login.java
@@ -0,0 +1,16 @@
+package Basic_Programs;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Firefox_Login {
+
+	public static void main(String[] args)
+	{
+		WebDriverManager.firefoxdriver().setup();
+		FirefoxDriver driver = new FirefoxDriver();
+
+	}
+
+}
